d8d4a3439f6082ad289d0b8ff5d67a4c91fb9a4a
diala <diala.sh.98@gmail.com>
2022-10-23 11:37:09 +0300

    scroll

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -5,12 +5,10 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div className=" bg-[#2b498e] h-16">
-      <div className="">
-        <Link href="/Main">
-          <Image src={Logo} className="cursor-pointer"   />
-        </Link>
-      </div>
+    <div className=" bg-[#2b498e] h-16 fixed w-full z-[40]">
+      <Link href="/Main">
+        <Image src={Logo} className="cursor-pointer" />
+      </Link>
     </div>
   );
 };
