d8200b7bb04d6366c993f96a0e7793105bdb7634
diala <diala.sh.98@gmail.com>
2022-11-01 09:27:28 +0200

    bg color

--- pages/components/MainHero.js
+++ pages/components/MainHero.js
@@ -14,7 +14,7 @@ const Hero = () => {
   return (
     <>
       {thank ? (
-        <div className="top-0 text-center bg-white dark:-mt-20">
+        <div className="top-0 pt-10 text-center bg-white dark:-mt-20">
           <div className="top-0 h-screen bg-white">
           <h1 className=" mx-10 mt-20 text-[20px] font-bold text-center text-black">
             We are glad to invite you to the opening of iSystem new branch in Abdoun on the 3rd of November at 6:00PM
