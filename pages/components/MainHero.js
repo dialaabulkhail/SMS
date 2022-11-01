1fc20b88172098a1f5901b68575d4c42c38cd3e3
diala <diala.sh.98@gmail.com>
2022-11-01 09:33:28 +0200

    final

--- pages/components/MainHero.js
+++ pages/components/MainHero.js
@@ -7,16 +7,16 @@ import Link from "next/link";
 import Footer from "./Footer";
 
 const Hero = () => {
-  const [thank, setThank] = useState(true);
+  const [thank, setThank] = useState(false);
   const [phone, setPhone] = useState("");
 
 
   return (
     <>
       {thank ? (
-        <div className="text-center bg-white dark:-mt-20 ">
+        <div className="-mt-20 text-center bg-white">
           <div className="top-0 h-screen bg-white">
-          <h1 className=" mx-10 dark:mt-60 mt-20 text-[20px] font-bold text-center text-black ">
+          <h1 className=" mx-10 mt-60  text-[20px] font-bold text-center text-black ">
             We are glad to invite you to the opening of iSystem new branch in Abdoun on the 3rd of November at 6:00PM
           </h1>
 
